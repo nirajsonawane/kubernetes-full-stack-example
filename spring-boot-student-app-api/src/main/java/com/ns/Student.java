f20c6e5aeddc35cb5b3cff175c7914aa276aacd4
Niraj Sonawane <niraj.sonawane@joonko.io>
2020-04-20 23:04:07 +0200

    Add mongodb and docker compose

--- spring-boot-student-app-api/src/main/java/com/ns/Student.java
+++ spring-boot-student-app-api/src/main/java/com/ns/Student.java
@@ -3,10 +3,16 @@ package com.ns;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
 
 @Data
+@Document
+
 public class Student {
 
+	@Id
 	private long id;
 	private String firstName;
 	private String lastName;
